6385998003c1de26bf13714aab41ac650d9b5900
lonla26 <cmoffolonla@gmail.com>
2022-04-23 13:19:31 +0100

    final commit of react redux project

--- src/components/ListTask.js
+++ src/components/ListTask.js
@@ -7,19 +7,19 @@ const ListTask = () => {
     if (displayTaskStyle === "all") {
         return (
             <div style={{ marginTop: "20px" }}>
-                {React.Children.toArray(todos.map(task => <Task task={task} />))}
+                {React.Children.toArray(todos.map(task => <Task task={task} key={task.id} />))}
             </div>
         )
     } else if(displayTaskStyle==="done"){
         return(
             <div style={{ marginTop: "20px" }}>
-            {React.Children.toArray(todos.filter(el=>el.isDone===true).map(task => <Task task={task} />))}
+            {React.Children.toArray(todos.filter(el=>el.isDone===true).map(task => <Task task={task} key={task.id} />))}
         </div>
         )
     } else{
         return(
             <div style={{ marginTop: "20px" }}>
-            {React.Children.toArray(todos.filter(el=>el.isDone===false).map(task => <Task task={task} />))}
+            {React.Children.toArray(todos.filter(el=>el.isDone===false).map(task => <Task task={task} key={task.id} />))}
         </div>
         )
     }
